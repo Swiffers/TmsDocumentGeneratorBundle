864a3bdb201080f35d0d7d422cf435855ad8ad2c
Jean-Philippe Chateau <contact@jpchateau.com>
2014-01-03 12:14:58 +0100

    first commit

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Jean-Philippe CHATEAU <jp.chateau@trepia.fr>
+ * @license: GPL
+ *
+ */
+
+namespace Tms\Bundle\DocumentGeneratorBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+
+        return $treeBuilder;
+    }
+}
