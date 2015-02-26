86896d987e7332ea44487455aad958fac9f02d9d
Linok75 <anritot@gmail.com>
2015-02-26 17:26:08 +0100

    Add generate() story line

--- Tests/Generator/GeneratorTest.php
+++ Tests/Generator/GeneratorTest.php
@@ -2,17 +2,53 @@
 
 namespace Tms\Bundle\DocumentGeneratorBundle\Tests\Generator;
 
-use PHPUnit_Framework_TestCase;
+use Tms\Bundle\DocumentGeneratorBundle\Generator\Generator;
+use Tms\Bundle\DocumentGeneratorBundle\DataFetcher\DataFetcherRegistry;
+use Tms\Bundle\DocumentGeneratorBundle\HtmlConverter\HtmlConverterRegistry;
 
 /**
  * Description of GeneratorTest
  *
  * @author Antoine Ribola <antoine.ribola@gmail.com>
  */
-class GeneratorTest extends PHPUnit_Framework_TestCase {
+class GeneratorTest extends \PHPUnit_Framework_TestCase {
+    /*
+     * Need to define all test values !
+     */
     public function testGenerate()
     {
-        //TO_DO
+        $generator = new Generator(
+            new HtmlConverterRegistry(),
+            new DataFetcherRegistry()
+        );
+        
+        //Default case (return pdf? html?)
+        $document = $generator->generate($template_id, $data);
+        $this->assertEquals($expectedDocument, $document);
+        
+        //Request pdf
+        $document = $generator->generate($template_id, $data, array("_format"=>"pdf"));
+        $this->assertEquals($expectedDocument, $document);
+        
+        //Request with data need to be fetch
+        $document = $generator->generate($template_id, $data);
+        $this->assertEquals($expectedDocument, $document);
+    }
+    
+    /**
+     * @expectedException MissingDataException
+     */
+    public function testMissingDataException(){
+        $generator = new Generator(
+            new HtmlConverterRegistry(),
+            new DataFetcherRegistry()
+        );
+        
+        //Constructor without data, useful ?
+        $document = $generator->generate($template_id);
+        
+        //Case with empty data
+        $document = $generator->generate($template_id, array());
     }
     
     public function testRender()
