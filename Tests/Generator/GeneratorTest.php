33955583b913c28cf63e3fccd668365da48eddf6
Linok75 <anritot@gmail.com>
2015-02-26 16:48:11 +0100

    Create a test file. (without story line)

---
+++ Tests/Generator/GeneratorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tms\Bundle\DocumentGeneratorBundle\Tests\Generator;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Description of GeneratorTest
+ *
+ * @author Antoine Ribola <antoine.ribola@gmail.com>
+ */
+class GeneratorTest extends PHPUnit_Framework_TestCase {
+    public function testGenerate()
+    {
+        //TO_DO
+    }
+    
+    public function testRender()
+    {
+        //TO_DO
+    }
+}
