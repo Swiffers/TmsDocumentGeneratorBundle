72b29e6195efb76939a3d27bf7b5080acf2624c2
Faust <shangguokan@hotmail.com>
2015-03-02 12:45:13 +0100

    update

--- Controller/Api/GeneratorController.php
+++ Controller/Api/GeneratorController.php
@@ -8,20 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class GeneratorController extends Controller
 {
-    public function testAction()
-    {
-       return new Response(
-           json_encode(
-                $this
-                    ->get('tms_document_generator.fetcher.registry')
-                    ->getDataFetcher('participation')
-                    ->fetch(
-                        array("participation_id"=>"52976d6fe63ea02c768b4567")
-                    )
-            )
-       );
-    }
-
     public function generateAction()
     {
 
